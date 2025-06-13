65b22cf7c6753f287783f0f4ac5993778aeb1ebc
farrel irawan <irawanssfarrel@gmail.com>
2025-06-13 17:46:50 +0700

    modifikasi layout profile card

--- src/app/profile/[id]/page.js
+++ src/app/profile/[id]/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image"
 import indoforumImage from "../../../../public/images/mosaic-indoforum.png"
 import fluidImage from "../../../../public/images/fluid.jpg"
+import { UserPlusIcon, MessageCircle } from "lucide-react"
 
 export default function UserProfile() {
     return (
@@ -22,14 +23,20 @@ export default function UserProfile() {
             {/* profile card */}
             <div className="w-full h-full px-5 pt-25">
                 {/* profile name and job */}
-                <div className="flex items-center justify-between">
+                <div className="flex flex-col gap-6">
                     <div className="flex flex-col items-start">
                         <h2 className="text-2xl font-m-bold capitalize">Farrel Irawan</h2>
                         <p className="text-sm font-m-medium">UI/UX Designer & Frontend Developer</p>
                     </div>
-                    <div className="flex justify-between gap-3">
-                        <button className="w-8 h-8 ring-2 rounded-full"></button>
-                        <button className="w-8 h-8 ring-2 rounded-full"></button>
+                    <div className="flex gap-3">
+                        <button className="w-32 ring-2 py-1.5 rounded-md flex items-center justify-center gap-2">
+                            <p className="text-base font-m-semibold">follow</p>
+                            <UserPlusIcon className="w-5 h-5" />
+                        </button>
+                        <button className="w-32 ring-2 py-1.5 rounded-md flex items-center justify-center gap-2">
+                            <p className="text-base font-m-semibold">message</p>
+                            <MessageCircle className="w-5 h-5" />
+                        </button>
                     </div>
                 </div>
             </div>
