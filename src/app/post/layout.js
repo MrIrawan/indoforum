b67bf4cbcbe494fbfc5a769bbae4460b84411f41
farrel irawan <irawanssfarrel@gmail.com>
2025-06-14 23:39:19 +0700

    membuat navbar post page (tahap awal)

--- src/app/post/layout.js
+++ src/app/post/layout.js
@@ -1,6 +1,8 @@
 import Image from "next/image"
 import indoforumLogo from "../../../public/images/indoforum-logo.svg"
-import { Search } from "lucide-react"
+import HomeButton from "@/components/ui/button/HomeButton"
+import ForumsButton from "@/components/ui/button/ForumsButton"
+import { Search, MessageSquareTextIcon } from "lucide-react"
 
 export default function PostLayout({ children }) {
     return (
@@ -10,15 +12,40 @@ export default function PostLayout({ children }) {
                     <div className="w-9 h-9 overflow-hidden">
                         <Image 
                             src={indoforumLogo}
+                            alt="indoforum-logo"
                         />
                     </div>
                     <form className="w-full flex items-center gap-3">
-                        <input type="text" placeholder="Cari di indoforum" className="w-full h-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none" />
+                        <input type="text" placeholder="Cari di indoforum" className="w-full h-full ring-2 ring-gray-400 font-m-medium rounded-md px-3 py-2 focus:outline-none placeholder:font-m-medium" />
                         <button type="submit" className="w-10 h-9 flex items-center justify-center ring-2 rounded-lg">
                             <Search className="w-7 h-7" />
                         </button>
                     </form>
                 </div>
+                <nav className="w-full h-15 bg-secondary-foreground px-3">
+                    <ul className="w-full h-full flex items-center gap-3 justify-around">
+                        <li>
+                            <a href="/post">
+                                <HomeButton />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/forums">
+                                <ForumsButton />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/post">
+                                <HomeButton />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/profile/123">
+                                <HomeButton />
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
             </header>
             { children }
         </>
---
+++ src/components/ui/button/ForumsButton.js
@@ -0,0 +1,9 @@
+import { MessageSquareTextIcon } from "lucide-react";
+
+export default function ForumsButton() {
+    return (
+        <button className="ring-2 rounded-lg p-1">
+            <MessageSquareTextIcon className="w-5 h-5" />
+        </button>
+    )
+}
