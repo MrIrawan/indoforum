fdc869ad5965b43301f607266aaf9f74106fbfeb
farrel irawan <irawanssfarrel@gmail.com>
2025-06-16 11:57:56 +0700

    modifikasi layout pada root component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,26 +1,27 @@
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata = {
   title: "Indoforum | Online Disscuss Platform",
   description: "An online discussion platform for everyone, can use to share ideas, opinions, and experiences.",
 };
 
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
