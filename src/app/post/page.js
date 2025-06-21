a27bcb37e595a30f3ce1a0687d6d05bfc2b26b07
farrel irawan <irawanssfarrel@gmail.com>
2025-06-21 11:19:06 +0700

    update UI postcard

--- src/app/post/page.js
+++ src/app/post/page.js
@@ -6,24 +6,14 @@ import inndofoumLogo from "../../../public/images/indoforum-logo.svg"
 export default function PostPage() {
     return (
         <>
-            <article className="w-full flex flex-col">
-                {/* card header (whos post and date) */}
-                <div className="w-full flex">
-                    <div className="w-fit flex flex-col gap-3">
-                        <div className="flex items-center h-12 gap-3">
-                            <Image 
-                                src={inndofoumLogo}
-                                alt="indoforum-logo"
-                                width={35}
-                                height={35}
-                                className="w-11 h-11 rounded-full"
-                            />
-                            <div className="flex flex-col">
-                                <p className="text-base font-m-semibold">Farrel Irawan</p>
-                                <p className="text-sm font-m-semibold text-gray-400">12 Februari 2025</p>
-                            </div>
-                        </div>
-                    </div>
+            <article className="w-full flex flex-col bg-gray-300 h-56">
+                {/* postcard parent */}
+                <div className="flex flex-col h-full w-full">
+                    {/* postcard item 1 */}
+
+                    {/* postcard item 2 */}
+
+                    {/* postcard item 3 */}
                 </div>
             </article>
         </>
