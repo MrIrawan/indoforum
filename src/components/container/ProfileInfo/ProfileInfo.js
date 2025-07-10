8173be5ce0158d419de03a61db512d114f9e5466
farrel irawan <irawanssfarrel@gmail.com>
2025-07-10 20:07:28 +0700

    update: mengubah struktur projek dan memecah komponen mengikuti konsep atomic design

--- src/components/container/ProfileInfo/ProfileInfo.js
+++ src/components/container/ProfileInfo/ProfileInfo.js
@@ -17,6 +17,7 @@ export default function UserProfileInfo() {
     const [userInfo, setUserInfo] = useState(null);
     const [additionalInfo, setAdditionalInfo] = useState(null);
     return (
+        <>
         <div className="w-full pt-6">
             <ul className="flex flex-col gap-3">
                 <li className="flex items-center gap-2">
@@ -31,62 +32,8 @@ export default function UserProfileInfo() {
                     <BriefcaseBusinessIcon className="w-5 h-5 text-primary-color" />
                     <p className="text-base font-m-medium">work at <span className="font-m-semibold">PT. DBS Bank</span></p>
                 </li>
-                <li className="flex items-center gap-2">
-                    <div className="w-full h-[250px] flex flex-col gap-2 justify-between">
-                        <div className="w-full h-full bg-white flex flex-col gap-3 justify-between py-3">
-                            <div className="flex items-center gap-2">
-                                <GraduationCapIcon className="w-6 h-6 text-primary-color"/>
-                                <div className="flex flex-col gap-0.5 w-full">
-                                    <label htmlFor="education" className="text-sm font-m-semibold">Your Education</label>
-                                    <input 
-                                        type="text"
-                                        placeholder="S1 Teknik Informatika"
-                                        className="text-base font-m-semibold ring-2 rounded-sm ring-gray-400 px-3 py-1.5 placeholder:font-m-semibold focus:outline-0"
-                                    />
-                                </div>
-                            </div>
-                            <div className="flex items-center gap-2">
-                                <GraduationCapIcon className="w-6 h-6 text-primary-color"/>
-                                <div className="flex flex-col gap-0.5 w-full">
-                                    <label htmlFor="education" className="text-sm font-m-semibold">Your Education</label>
-                                    <input 
-                                        type="text"
-                                        placeholder="S1 Teknik Informatika"
-                                        className="text-base font-m-semibold ring-2 rounded-sm ring-gray-400 px-3 py-1.5 placeholder:font-m-semibold focus:outline-0"
-                                    />
-                                </div>
-                            </div>
-                            <div className="flex items-center gap-2">
-                                <GraduationCapIcon className="w-6 h-6 text-primary-color"/>
-                                <div className="flex flex-col gap-0.5 w-full">
-                                    <label htmlFor="education" className="text-sm font-m-semibold">Your Education</label>
-                                    <input 
-                                        type="text"
-                                        placeholder="S1 Teknik Informatika"
-                                        className="text-base font-m-semibold ring-2 rounded-sm ring-gray-400 px-3 py-1.5 placeholder:font-m-semibold focus:outline-0"
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                        <hr className="w-full h-1 border border-gray-400" />
-                        <div className="w-full h-4/12 bg-white flex items-center gap-3 py-3">
-                            <Button
-                                buttonStyle="ring-2 w-42 py-1.5 rounded-md flex items-center justify-center gap-2"
-                            >
-                                <p className="text-base font-m-semibold text-black">save changes</p>
-                                <CheckIcon className="w-6 h-6 text-green-500" />
-                            </Button>
-                            <Button
-                                buttonStyle="ring-2 w-46 py-1.5 rounded-md flex items-center justify-center gap-2"
-                            >
-                                    <p className="text-base font-m-semibold text-black">discard changes</p>
-                                    <Trash2Icon className="w-6 h-6 text-red-500" />
-                                </Button>
-                        </div>
-                    </div>
-                </li>
-                {/* additional info */}
             </ul>
         </div>
+        </>
     )
 }
--- src/components/pages/UserProfilePage.js
+++ src/components/pages/UserProfilePage.js
@@ -5,12 +5,14 @@ import { useState } from "react"
 import UserProfileLayout from "../layout/UserProfileLayout";
 import ProfileNameLayout from "../layout/ProfileNameLayout";
 import UserProfileInfo from "../container/ProfileInfo/ProfileInfo";
+import UserProfileActivity from "../container/ProfileActivity/ProfileActivity";
 
 export default function UserProfilePage() {
     return (
         <UserProfileLayout>
             <ProfileNameLayout />
             <UserProfileInfo />
+            <UserProfileActivity />
         </UserProfileLayout>
     )
 }
