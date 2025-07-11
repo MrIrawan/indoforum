d134065ba5d2caff4cbdaf402e83d7d2edf9822f
farrel irawan <irawanssfarrel@gmail.com>
2025-07-11 07:37:08 +0700

    mengganti komponen halaman user dengan halaman user yang baru (structuralChange)

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,8 @@
-import UserProfileInfo from "@/components/container/ProfileInfo/ProfileInfo";
-import UserProfilePage from "@/components/pages/UserProfilePage";
 
 export default function Home() {
   return (
     <>
-      <UserProfilePage />
+      
     </>
   );
 }
--- src/app/profile/[id]/page.js
+++ src/app/profile/[id]/page.js
@@ -1,14 +1,9 @@
 "use client"
 
-import ProfileCard from "@/components/ui/profile/ProfileCard"
+import UserProfilePage from "@/components/pages/UserProfilePage"
 
 export default function UserProfile() {
     return (
-        <ProfileCard>
-            <ProfileCard.NameAndJob />
-            <ProfileCard.ProfileInfo />
-            <ProfileCard.ProfileDescription name="Farrel" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula consectetur tempus. Phasellus in egestas libero. Praesent at ligula eget lectus commodo facilisis. Sed molestie sodales ipsum. Phasellus bibendum volutpat scelerisque. Nulla faucibus quam sit amet."/>
-            <ProfileCard.ProfileActivity />
-        </ProfileCard>
+        <UserProfilePage />
     )
 }
