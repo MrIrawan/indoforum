f4e3a3b54cec2f9ceda6733b6c77b8cf4c75de7b
farrel irawan <irawanssfarrel@gmail.com>
2025-07-22 00:11:06 +0700

    membuat container QuestionByUser dan QuestionFooter

---
+++ src/components/container/QuestionByUser/QuestionByUser.js
@@ -0,0 +1,16 @@
+import Avatar from "@/components/ui/Avatar/Avatar";
+
+export default function QuestionByUser({ userData }) {
+    return (
+        <div className="flex items-center gap-2.5">
+            <Avatar avatarSize="default" />
+            <div className="flex flex-col">
+                <div className="flex items-center gap-2">
+                    <h3 className="text-base font-m-semibold">Question By</h3>
+                    <hr className="w-2 h-2 bg-blue-500 border-none rounded-full"/>
+                    <h3 className="text-base font-m-semibold">{ userData.user_name ? userData.user_name : "Indoforum User" }</h3>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/container/QuestionFooter/QuestionFooter.js
@@ -0,0 +1,11 @@
+export default function QuestionFooter({ answeredCount = 0 }) {
+    return (
+        <div className="bg-white w-full flex items-center justify-between mt-6">
+            <div className="flex items-center gap-2">
+                <p className="text-sm font-m-semibold">Answered By</p>
+                <hr className="w-2 h-2 bg-blue-500 border-none rounded-full"/>
+                <p className="text-sm font-m-semibold">{answeredCount} User's</p>
+            </div>
+        </div>
+    )
+}
