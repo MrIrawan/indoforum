38d053c0f4266dc7a12e13be2088f1482c29be30
farrel irawan <irawanssfarrel@gmail.com>
2025-07-21 22:55:20 +0700

    membuat page dan layout untuk route questions dan membuat kerangka awal komponen question card

---
+++ src/app/questions/layout.js
@@ -0,0 +1,9 @@
+import SectionSite from "@/components/layout/Section";
+
+export default function QuestionsPageLayout({ children }) {
+    return (
+        <SectionSite>
+            { children }
+        </SectionSite>
+    )
+}
